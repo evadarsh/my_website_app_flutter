2724e28103a90839b05e6ca2a927ab806a50050c
Adarsh <adarshev2002@gmail.com>
2024-06-27 21:35:13 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WebPage(),
+    );
+  }
+}
+
+class WebPage extends StatefulWidget {
+  @override
+  _WebPageState createState() => _WebPageState();
+}
+
+class _WebPageState extends State<WebPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebView(
+        initialUrl: 'https://adarshev.bio.link/',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
